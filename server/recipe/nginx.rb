998a22c1bc39bc04df5b1310408ed78d1abdbbdb
Konboi <ryosuke.yabuki@gmail.com>
2015-03-02 21:36:22 +0900

    docker用のnginxファイルを追加

--- server/recipe/nginx.rb
+++ server/recipe/nginx.rb
@@ -3,3 +3,13 @@ package "nginx"
 service "nginx" do
   action :enable
 end
+
+directory "/etc/nginx/conf.d" do
+  owner "root"
+  group "root"
+  mode  "0755"
+end
+
+template "/etc/nginx/conf.d/docker.wakateweb.konboi.com.conf" do
+  source 'docker.wakateweb.konboi.com.conf.erb'
+end
